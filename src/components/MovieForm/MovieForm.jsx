ffb4cd5ab9263b50a6b08c8a4e64170c1912b160
Solomon Lambert <sol.lambert@gmail.com>
2023-03-04 19:38:58 -0600

    add select element for genres and get route on server to supply genres to front end

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function MovieForm() {
 
     const dispatch = useDispatch();
+    const genres = useSelector(store => store.genres);
+
+    useEffect(() => {
+        dispatch({type: 'FETCH_GENRES'});
+    }, []);
 
     const [input, setInput] = useState({
         title: '',
         description: '',
         poster: '',
-    })
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,6 +24,10 @@ function MovieForm() {
 
     const handleChange = (e, key) => {
         setInput({ ...input, [key]: e.target.value });
+    }
+
+    const handleSelect = (e) => {
+        setInput({...input, genre_id: e.target.value});
     }
 
     const clearInput = () => {
@@ -37,12 +46,17 @@ function MovieForm() {
                         onChange={e => handleChange(e, 'title')} />
                 </div>
                 <div>
-                    <textarea rows="5" cols="45" value={input.description} placeholder="Description..."
+                    <textarea rows="5" cols="40" value={input.description} placeholder="Description..."
                         onChange={e => handleChange(e, 'description')}></textarea>
                 </div>
                 <input type="text" placeholder="Poster URL" value={input.poster}
                     onChange={e => handleChange(e, 'poster')} />
                 <input type="submit" value="SUBMIT" onClick={handleSubmit} />
+                <select onChange={handleSelect}>
+                    {genres.map((genre) => {
+                        return <option value={genre.id}>{genre.name}</option>
+                    })}
+                </select>
             </form>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
+    yield takeEvery('FETCH_GENRES', fetchAllGenres);
     yield takeEvery('FETCH_MOVIES', fetchAllMovies);
     yield takeEvery('FETCH_MOVIE_DETAILS', fetchMovieDetails);
 }
@@ -31,13 +32,20 @@ function* fetchAllMovies() {
     // get all movies from the DB
     try {
         const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
         yield put({ type: 'SET_MOVIES', payload: movies.data });
-
     } catch {
         console.log('get all error');
     }
         
+}
+
+function* fetchAllGenres() {
+    try {
+        const genres = yield axios.get('/api/genre');
+        yield put({ type: 'SET_GENRES', payload: genres.data});
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // Create sagaMiddleware
