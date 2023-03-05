89507da7c1105d2492b7065403548691ac08997e
Solomon Lambert <sol.lambert@gmail.com>
2023-03-05 14:26:14 -0600

    mild formatting changes and labels

--- src/components/MovieDetails/EditDetails/EditDetails.jsx
+++ src/components/MovieDetails/EditDetails/EditDetails.jsx
@@ -4,12 +4,12 @@ import { useDispatch } from "react-redux";
 
 function EditDetails({ clearDetails, movieDetails, handleEditing }) {
 
+    const dispatch = useDispatch();
+    const [values, setValues] = useState({ title: '', description: '', poster: ''});
+
     useEffect(() => {
         setValues(movieDetails);
-    },[])
-    
-    const dispatch = useDispatch();
-    const [values, setValues] = useState({ title: '', description: '' });
+    }, [])
 
     //handle input change
     const handleChange = (e, key) => {
@@ -19,27 +19,34 @@ function EditDetails({ clearDetails, movieDetails, handleEditing }) {
     //submit the new changes for the movie to be updated on db
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch({type: "SUBMIT_DETAIL_EDIT", payload: values, callback: finishSubmit});
+        dispatch({ type: "SUBMIT_DETAIL_EDIT", payload: values, callback: finishSubmit });
     }
 
     //callback for the SUBMIT_DETAIL_EDIT saga
     const finishSubmit = () => {
         handleEditing();
         clearDetails();
-        dispatch({type: "CLEAR_MOVIE_LIST"});
+        dispatch({ type: "CLEAR_MOVIE_LIST" });
     }
-    
+
     return (
         <div>
             <form>
                 <div>
-                    <input type="submit" value="Save Entry" onClick={handleSubmit}/>
+                    <input type="submit" value="Save Entry" onClick={handleSubmit} />
                     <input type="button" value="Cancel Edit" onClick={handleEditing} />
                 </div>
                 <div>
-                    <input type="text"
+                    <label htmlFor="title">TITLE: </label>
+                    <input name="title" type="text"
                         value={values.title}
                         onChange={e => handleChange(e, 'title')} />
+                </div>
+                <div>
+                    <label htmlFor="poster">POSTER: </label>
+                    <input name="poster" type="text"
+                        value={values.poster}
+                        onChange={e => handleChange(e, 'poster')} />
                 </div>
                 <img src={movieDetails.poster} width='185' height='274' />
                 <ul>
@@ -48,7 +55,11 @@ function EditDetails({ clearDetails, movieDetails, handleEditing }) {
                     })}
                 </ul>
                 <div>
+                    <div>
+                        <label htmlFor="description">DESCRIPTION</label>
+                    </div>
                     <textarea
+                        name="description"
                         rows="10"
                         cols="30"
                         onChange={e => handleChange(e, 'description')}
